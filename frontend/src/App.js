8f14aeb6aac343508813a545dab7769877e8efe6
hendrikdgh <hendrik.dgh@gmail.com>
2023-10-15 00:47:46 +1100

    Interface4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,11 @@ import Header from './Header';
 function App() {
     const [contacts, setContacts] = useState([]);
     const [selectedContact, setSelectedContact] = useState(null);  // Store the clicked contact
+    const [showStats, setShowStats] = useState(false);
+
+    const toggleStats = () => {
+        setShowStats(!showStats);
+    };
 
     const addContact = (name) => {
         if (name.trim() !== '') {
@@ -28,6 +33,13 @@ function App() {
         const newContacts = [...contacts];
         newContacts[contactIndex].phones.splice(phoneIndex, 1);
         setContacts(newContacts);
+    };
+
+    const stats = {
+        numberOfContacts: contacts.length,
+        numberOfPhones: contacts.reduce((acc, contact) => acc + contact.phones.length, 0),
+        newestContactTimestamp: contacts.length ? contacts[contacts.length - 1].timestamp : '',
+        oldestContactTimestamp: contacts.length ? contacts[0].timestamp : ''
     };
 
     return (
@@ -58,6 +70,20 @@ function App() {
                     </div>
                 ))}
             </div>
+
+            <button onClick={toggleStats}>
+                {showStats ? "Hide Stats" : "Show Stats"}
+            </button>
+
+            {showStats && (
+                <div className="stats-section">
+                    <h2>Statistics</h2>
+                    <p>Number of Contacts: {stats.numberOfContacts}</p>
+                    <p>Number of Phones: {stats.numberOfPhones}</p>
+                    <p>Newest Contact Timestamp: {stats.newestContactTimestamp}</p>
+                    <p>Oldest Contact Timestamp: {stats.oldestContactTimestamp}</p>
+                </div>
+            )}
         </div>
     );
 }
