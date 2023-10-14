2a5b627fb5273c38d8a6f4f792f54e6d33687ab3
hendrikdgh <hendrik.dgh@gmail.com>
2023-10-14 23:36:09 +1100

    Interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,5 @@
 import React, { useEffect, useState } from 'react'; // Make sure to import useEffect
 import './App.css';
-import ContactInput from './ContactInput';
-import Contacts from './Contacts';
-import Header from './Header';
 
 function App() {
     const [contacts, setContacts] = useState([]); // State to maintain contacts list
@@ -22,9 +19,47 @@ function App() {
 
     return (
         <div>
-            <Header />
-            <ContactInput addContact={addContact} />
-            <Contacts contacts={contacts} />
+            <h1>Contactor</h1>
+            <div>
+                <input
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    placeholder="Enter Name"
+                />
+                <button onClick={handleAddContact}>Add Contact</button>
+            </div>
+            <div>
+                {contacts.map(contact => (
+                    <div key={contact.name}>
+                        <span>{contact.name}</span>
+                        <button onClick={() => handleDeleteContact(contact.name)}>Delete</button>
+                        {selectedContact === contact.name && (
+                            <div>
+                                <input
+                                    value={phone}
+                                    onChange={e => setPhone(e.target.value)}
+                                    placeholder="Enter Phone Number"
+                                />
+                                <button onClick={handleAddPhone}>Add Phone</button>
+                            </div>
+                        )}
+                        <button onClick={() => setSelectedContact(contact.name)}>View Phones</button>
+                    </div>
+                ))}
+            </div>
+            <div>
+                <button>Show Stats</button>
+                {selectedContact && (
+                    <div>
+                        <h2>{selectedContact}</h2>
+                        <ul>
+                            {contacts.find(c => c.name === selectedContact).phones.map(phone => (
+                                <li key={phone}>{phone}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
