a36161d8cdf990f4aece75dfcb9cb322009e005c
hendrikdgh <hendrik.dgh@gmail.com>
2023-10-15 00:38:32 +1100

    Interface4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,63 @@
-import { useState } from 'react'; // import useEffect
+import React, { useState } from 'react';
 import './App.css';
-import ContactInput from './ContactInput';
-import Contacts from './Contacts';
 import Header from './Header';
 
 function App() {
-    const [contacts, setContacts] = useState([]); // State to maintain contacts list
+    const [contacts, setContacts] = useState([]);
+    const [selectedContact, setSelectedContact] = useState(null);  // Store the clicked contact
 
     const addContact = (name) => {
-        // Logic to add contact
-        // Here, you could also make an API call to POST the contact to your backend
-        setContacts([...contacts, { name, phones: [] }]);
+        if (name.trim() !== '') {
+            setContacts([...contacts, { name, phones: [] }]);
+        }
+    };
+
+    const deleteContact = (index) => {
+        const newContacts = [...contacts];
+        newContacts.splice(index, 1);
+        setContacts(newContacts);
+    };
+
+    const addPhoneNumber = (index, type, number) => {
+        const newContacts = [...contacts];
+        newContacts[index].phones.push({ type, number });
+        setContacts(newContacts);
+    };
+
+    const deletePhoneNumber = (contactIndex, phoneIndex) => {
+        const newContacts = [...contacts];
+        newContacts[contactIndex].phones.splice(phoneIndex, 1);
+        setContacts(newContacts);
     };
 
     return (
         <div>
             <Header />
-            <ContactInput addContact={addContact} />
-            <Contacts contacts={contacts} />
+            <div className="input-section">
+                <input type="text" placeholder="Name" id="contact-name-input" />
+                <button onClick={() => addContact(document.getElementById("contact-name-input").value)}>Create Contact</button>
+            </div>
+            <div>
+                {contacts.map((contact, index) => (
+                    <div key={index}>
+                        <span className="contact-name" onClick={() => setSelectedContact(index === selectedContact ? null : index)}>{contact.name}</span>
+                        <button onClick={() => deleteContact(index)}>Delete</button>
+                        {index === selectedContact && (
+                            <div className="phone-input-section">
+                                <input type="text" placeholder="Name" id={`type-input-${index}`} />
+                                <input type="text" placeholder="Phone Number" id={`phone-input-${index}`} />
+                                <button onClick={() => addPhoneNumber(index, document.getElementById(`type-input-${index}`).value, document.getElementById(`phone-input-${index}`).value)}>Add</button>
+                            </div>
+                        )}
+                        {contact.phones.map((phone, phoneIndex) => (
+                            <div key={phoneIndex}>
+                                <span>{phone.type}: {phone.number}</span>
+                                <button onClick={() => deletePhoneNumber(index, phoneIndex)}>Delete</button>
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
