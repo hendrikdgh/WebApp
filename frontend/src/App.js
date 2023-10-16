eaa0f768902ab90927ccaa24dbb5fa02e3f23566
hendrikdgh <hendrik.dgh@gmail.com>
2023-10-16 20:03:08 +1100

    Interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
     const [showStats, setShowStats] = useState(false);
 
     useEffect(() => {
-        fetch('http://localhost:5000/api/contacts')
+        fetch('http://localhost:5000/api/contacts/')
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
